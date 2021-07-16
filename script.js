02883e2c25653afa4ec61e6feb266795b70b58cf
Ranajith Reddy <59986451+ranajithreddy@users.noreply.github.com>
2021-07-16 20:05:21 +0530

    Nami

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+
+window.onscroll=function(){
+	const docScrollTop=document.documentElement.scrollTop;
+	if(window.innerWidth>991){
+		if(docScrollTop>100){
+			document.querySelector("header").classList.add("fixed")
+		}
+		else{
+			document.querySelector("header").classList.remove("fixed")
+		}
+	}
+}
+
+
